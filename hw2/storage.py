e617b581b540a68da02c5e12b5373ffd89b4351b
Rustem <hrustim25@gmail.com>
2024-12-12 22:20:08 +0300

    thread safety + more ops on storage

--- hw2/storage.py
+++ hw2/storage.py
@@ -1,22 +1,67 @@
 import typing
+import threading
 
 class Storage:
     _storage: typing.Dict[str, typing.Any]
+    _rlock: threading.RLock
 
     def __init__(self):
         self._storage = {}
+        self._rlock = threading.RLock()
 
-    def getValue(self, key: str) -> typing.Any:
-        if key not in self._storage:
-            return None
-        return self._storage[key]
+    def get_value(self, key: str) -> typing.Any:
+        self._rlock.acquire(blocking=False)
+        try:
+            if key not in self._storage:
+                return None
+            return self._storage[key]
+        finally:
+            self._rlock.release()
+
+    def create(self, key: str) -> bool:
+        self._rlock.acquire()
+        try:
+            if key in self.storage[key]:
+                return False
+            self._storage[key] = None
+            return True
+        finally:
+            self._rlock.release()
+
+    def insert(self, key: str, value: typing.Any) -> bool:
+        self._rlock.acquire()
+        try:
+            if key in self.storage[key]:
+                return False
+            self._storage[key] = value
+            return True
+        finally:
+            self._rlock.release()
 
     def upsert(self, key: str, value: typing.Any) -> typing.Any:
-        old_value = self.storage[key]
-        self._storage[key] = value
-        return old_value
+        self._rlock.acquire()
+        try:
+            old_value = self.storage[key]
+            self._storage[key] = value
+            return old_value
+        finally:
+            self._rlock.release()
     
     def delete(self, key: str) -> typing.Any:
-        old_value = self.storage[key]
-        del self._storage[key]
-        return old_value
+        self._rlock.acquire()
+        try:
+            old_value = self.storage[key]
+            del self._storage[key]
+            return old_value
+        finally:
+            self._rlock.release()
+
+    def cas(self, key: str, value: typing.Any, expected: typing.Any) -> bool:
+        self._rlock.acquire()
+        try:
+            if key not in self._storage or self._storage[key] != expected:
+                return False
+            self._storage[key] = value
+            return True
+        finally:
+            self._rlock.release()
